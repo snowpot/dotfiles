d27f299f87b96dfe36120b74eb95e19b6f4860c1
snowpot <yukiwork.125@gmail.com>
2025-04-15 00:31:43 +0900

    update: nvim

---
+++ nvim/lua/moon/lazy/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "javascript",
+          "typescript",
+          "lua",
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+          disable = { "python", "c" } -- these and some other langs don't work well
+        },
+      })
+  end
+}
