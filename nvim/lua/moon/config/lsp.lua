deb4c97fd8053303debad777db195729bb888129
yk1r <yukiwork.125@gmail.com>
2025-04-18 16:54:02 +0900

    update: lsp

--- nvim/lua/moon/config/lsp.lua
+++ nvim/lua/moon/config/lsp.lua
@@ -17,7 +17,30 @@ mason_lspconfig.setup({
 })
 
 local capabilities =  require("cmp_nvim_lsp").default_capabilities()
-local builtin = require("telescope.builtin")
+
+vim.diagnostic.config({
+  virtual_text = true,
+  float = { border = "rounded" },
+  underline = true,
+  severity_sort = true,
+  signs = {
+    text = {
+   [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN]  = "",
+      [vim.diagnostic.severity.HINT]  = "",
+      [vim.diagnostic.severity.INFO]  = "",    }
+  }
+})
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  { border = "rounded" }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  { border = "rounded" }
+)
 
 -- 各LSPのセットアップ
 mason_lspconfig.setup_handlers({
@@ -26,15 +49,15 @@ mason_lspconfig.setup_handlers({
       capabilities = capabilities,
       on_attach = function(_, bufnr)
         local map_opts = { buffer = bufnr, noremap = true, silent = true }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, map_opts)           -- 定義へ
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, map_opts)         -- 宣言へ
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation, map_opts)     -- 実装へ
-vim.keymap.set("n", "gr", vim.lsp.buf.references, map_opts)         -- 参照へ
-vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, map_opts)    -- 型定義へ
-vim.keymap.set("n", "K", vim.lsp.buf.hover, map_opts)               -- ドキュメント
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, map_opts)     -- 名前変更
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, map_opts)-- アクション
-vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, map_opts) -- 診断表示
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, map_opts)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, map_opts)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, map_opts)
+vim.keymap.set("n", "gr", vim.lsp.buf.references, map_opts)
+vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, map_opts)
+vim.keymap.set("n", "K", vim.lsp.buf.hover, map_opts)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, map_opts)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, map_opts)
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, map_opts)
       end,
     }
 
