a25b2837ed00c46b420815e4c1b7cf4c98741eaa
yk1r <yukiwork.125@gmail.com>
2025-05-01 18:03:41 +0900

    update: nvim

--- nvim/lua/moon/config/lsp.lua
+++ nvim/lua/moon/config/lsp.lua
@@ -1,5 +1,6 @@
-local lspconfig = require("lspconfig")
 local mason_lspconfig = require("mason-lspconfig")
+local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 local servers = {
 	ts_ls = {},
 	lua_ls = {
@@ -11,12 +12,11 @@ local servers = {
 		},
 	},
 }
--- Masonで自動インストール
+
 mason_lspconfig.setup({
 	ensure_installed = vim.tbl_keys(servers),
 })
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
 vim.diagnostic.config({
 	virtual_text = true,
 	float = { border = "rounded" },
@@ -36,30 +36,33 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { 
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
-mason_lspconfig.setup_handlers({
-	function(server_name)
-		local opts = {
-			capabilities = capabilities,
-			on_attach = function(client, bufnr)
-				client.server_capabilities.semanticTokensProvider = nil
-				local map_opts = { buffer = bufnr, noremap = true, silent = true }
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, map_opts)
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, map_opts)
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, map_opts)
-				vim.keymap.set("n", "gr", vim.lsp.buf.rename, map_opts)
-				vim.keymap.set("n", "gR", vim.lsp.buf.references, map_opts)
-				vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, map_opts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, map_opts)
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, map_opts)
-				vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, map_opts)
-			end,
-		}
+local function on_attach(client, bufnr)
+	client.server_capabilities.semanticTokensProvider = nil
+	local map_opts = { buffer = bufnr, noremap = true, silent = true }
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, map_opts)
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, map_opts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, map_opts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.rename, map_opts)
+	vim.keymap.set("n", "gR", vim.lsp.buf.references, map_opts)
+	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, map_opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, map_opts)
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, map_opts)
+	vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, map_opts)
+end
 
-		-- merge server-specific options
-		if servers[server_name] then
-			opts = vim.tbl_deep_extend("force", opts, servers[server_name])
-		end
-
-		lspconfig[server_name].setup(opts)
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		client.server_capabilities.semanticTokensProvider = nil
 	end,
 })
+
+vim.lsp.config("*", {
+	capabilities = cmp_capabilities,
+	on_attach = on_attach,
+})
+
+for name, cfg in pairs(servers) do
+	vim.lsp.config(name, cfg)
+	vim.lsp.enable(name)
+end
